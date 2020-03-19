f4ccc7f8b6cb925f1279cc58165f06eee74a5230
unknown <administrator@SOUTHERSON.SE>
2020-03-19 17:37:08 +0100

    Första committen

---
+++ calc.py
@@ -0,0 +1,91 @@
+"""A = int(input("Vad är tal A?"))
+B = int(input("Vad är tal B?"))
+summa = int(A / B)
+
+print(summa)"""
+
+
+"""A = input("Vad är text A?")
+B = input("Vad är text B?")
+tumma = A + B
+
+print(tumma)""" 
+
+"""
+from math import pi
+
+radie = int(input("Radien på cirkeln är: "))
+
+Area = pi*(radie*radie)
+
+print(Area)"""
+
+"""H = int(input("Vad här höjden? "))
+Bas = int(input("Vad här basen? "))
+
+Area = (Bas*H)/2 
+
+print(Area, "cm2 är arean", H, "är höjden", Bas, "är basen.")"""
+
+"""x = int(input("Talet X är:"))
+y = int(input("Talet Y är: "))
+
+sum = (x+y)*(x+y)
+
+print(sum)"""
+
+"""rest = 455%60
+timmar = (455-rest)/60
+
+
+print(timmar,"timmmar och ", rest, "minuter.")
+"""
+
+"""antal_sekunder = int(input("Ge mig x antal sekunder: "))
+
+antal_dagar = (antal_sekunder-(antal_sekunder % 86400))/86400
+antal_timmar = (antal_sekunder-(antal_sekunder % 3600))/3600
+antal_minuter = (antal_sekunder-(antal_sekunder % 60))/60
+
+print (antal_dagar, " antal dagar,", antal_timmar," antal timmar,", antal_minuter, " antal minuter.")
+"""
+"""
+
+x,y,z = 3, 4, 5
+
+print(x,y,z)"""
+
+"""
+a = "A"
+b = "B"
+
+
+print(a, b)
+"""
+"""name = input("Ditt namn?\n")
+age = int(input("Vilket hår många år är du?"))
+
+current_year= 2020
+
+born = current_year - age
+
+hundra = born + 100
+
+print(name,".\n", born, " är årdet du är född.", hundra, "är året du fyller 100år!")
+
+"""
+"""nr = int(input("Nr tack: "))
+nr = nr%2
+if nr == 1:
+    print("Ojämt")
+elif nr == 0:
+    print("Jämt")
+else:
+    print("knas")"""
+"""
+Ord = input("Nr tack: ")
+if Ord == "hej":
+    print("Rätt ord")
+else:
+    print("Fel ord!")"""
+
